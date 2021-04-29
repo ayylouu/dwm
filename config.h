9093c32185b5341fec1c4e35bec8d966337723f4
louay <ayyl0uu@pm.me>
2021-04-29 10:39:54 +0100

    update

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static int swallowfloating          = 0;
 int gappx                           = 2;
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=15:antialias=true:autohint=true"  };
 static const char dmenufont[]       = { "monospace:size=10" };
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
